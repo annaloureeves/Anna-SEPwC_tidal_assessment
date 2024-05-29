5b3c152fc77dd3f5eec66c7c528d59da6334d7c6
annaloureeves <166381157+annaloureeves@users.noreply.github.com>
2024-05-29 19:54:36 +0100

    Lint passed

--- tidal_analysis.py
+++ tidal_analysis.py
@@ -7,7 +7,6 @@ import argparse
 import glob
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.stats import linregress
 from matplotlib.dates import date2num
 import uptide
@@ -19,29 +18,41 @@ def isnumber(x):
     try:
         float(x)
         return True
-    except:
+    except ValueError:
+        return False
+    except TypeError:
         return False
 
 def read_tidal_data(filename):
     """Reads in selected file name of standard formatting and exports to a data frame"""
-    tidal_file = "data/1947ABE.txt"
-    # Read data, separate columns by spaces, ignore first 10 , rename data column as "Sea Level"
+    # Read the CSV file into a DataFrame, skipping specific rows and using whitespace as the delimiter
     # References: https://www.geeksforgeeks.org/how-to-read-space-delimited-files-in-pandas/
     # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html
     # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html
     data = pd.read_csv(filename, skiprows=[
                     0, 1, 2, 3, 4, 5, 6, 7, 8, 10], sep=r'\s+')
+    # Rename column to "Sea Level"
     data.rename(columns={data.columns[3]: "Sea Level"}, inplace=True)
     # Combine "Date" and "Time" to "datetimes"
     # References: https://stackoverflow.com/questions/17978092/combine-date-and-time-columns-using-pandas
     # https://pandas.pydata.org/docs/reference/api/pandas.DatetimeIndex.html
     data['datetime'] = pd.to_datetime(data['Date'] + ' ' + data['Time'])
+    # Set "datetime" column as the index of the df
+    # https://stackoverflow.com/questions/27032052/how-do-i-properly-set-the-datetimeindex-for-a-pandas-datetime-object-in-a-datafr
     data = data.set_index('datetime')
+    # Filter to only include rows where all the elements are numbers
     data = data[data.map(isnumber)]
+    # Convert "Sea Level" to float type
+    # https://stackoverflow.com/questions/16729483/converting-strings-to-floats-in-a-dataframe
     data["Sea Level"] = data["Sea Level"].astype(float)
+    # Convert "Residual" to float type
     data["Residual"] = data["Residual"].astype(float)
+    # Replace values in column 'A' that end with 'M' with NaN
+    # https://github.com/jhill1/SEPwC_tidal_assessment
     data.replace(to_replace=".*M$",value={'A':np.nan},regex=True,inplace=True)
+    # Replace values in column 'A' that end with 'N' with NaN
     data.replace(to_replace=".*N$",value={'A':np.nan},regex=True,inplace=True)
+    # Replace values in column 'A' that end with 'T' with NaN
     data.replace(to_replace=".*T$",value={'A':np.nan},regex=True,inplace=True)
     return data
 
@@ -49,12 +60,17 @@ def extract_single_year_remove_mean(year, data):
     """Extracts single year and subtracts the mean from each row"""
     # Function based on work from:
     # https://www.dataquest.io/blog/datetime-in-pandas/
+    # Create a string representing the start of the year in 'YYYYMMDD' format
     year_string_start = str(year)+"0101"
+     # Create a string representing the end of the year in 'YYYYMMDD' format
     year_string_end = str(year)+"1231"
+    # Select "Sea Level" data from the DataFrame 'data' for a given year
     year_data = data.loc[year_string_start:year_string_end, ['Sea Level']]
     # Find the mean and remove it
     # https://jhill1.github.io/SEPwC.github.io/Mini_courses.html#tidal-analysis-in-python
+    # Calculate the mean of the "Sea Level" for the selected year
     mmm = np.mean(year_data['Sea Level'])
+    # Subtract the mean 'Sea Level' from the 'Sea Level' column
     year_data['Sea Level'] -= mmm
     return year_data
 
@@ -68,34 +84,49 @@ def extract_section_remove_mean(start, end, data):
 
 def join_data(data1, data2):
     """Joins data frames in chronological order"""
+    # Concatenate data1 and data2 DataFrames vertically
     joined_data = pd.concat([data1, data2])
+    # Sort the concatenated DataFrame by its index in ascending order
     # https://stackoverflow.com/questions/40262710/sorting-a-pandas-dataframe-by-its-index
     joined_data.sort_index(ascending=True, inplace=True)
     return joined_data
 
+def convert_date_to_number(x):
+    """Convert date to a number"""
+    return date2num(x)
 
 def sea_level_rise(data):
-    """Linnear Regression of Sea Level Against Time"""
+    """Linear Regression of Sea Level Against Time"""
     # https://www.w3schools.com/python/python_ml_multiple_regression.asp
     # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.dropna.html
+    # Assign the DataFrame 'data' to the variable 'df'
     df = data
-    df['datetime_as_number'] = df.index.map(lambda x: date2num(x))
+    # Convert datetime index to numerical format using date2num
+    df['datetime_as_number'] = df.index.map(convert_date_to_number)
+    # Drop rows with missing values in 'Sea Level'
     df.dropna(subset=["Sea Level"], inplace=True)
+    # Assign new datetime values to 'x' and 'Sea Level' values to 'y'
     x = df["datetime_as_number"]
     y = df['Sea Level']
-    res = linregress(x, y)
-    return res.slope, res.pvalue
+    # Perform linear regression
+    slope, _, _, pvalue, _ = linregress(x, y)
+    return slope, pvalue
 
 def tidal_analysis(data, constituents, start_datetime):
     """Harmonic Analysis of Sea Level Data Using Tidal Constituents"""
     # using uptide package as described
     # https://github.com/stephankramer/uptide
+    # Drop 'Sea Level" rows with missing values
     data = data.dropna(subset=["Sea Level"])
     # https://stackoverflow.com/questions/16628819/convert-pandas-timezone-aware-datetimeindex-to-naive-timestamp-but-in-certain-t
     # Putting this here because otherwise if we put it in read data, the tests fail
+    # Convert the index to a datetime format using UTC timezone
     data.index = pd.to_datetime(data.index, utc=True)
+    # Tides object with specified constituents
     tide = uptide.Tides(constituents)
+    # Set initial time to given start_datetime
     tide.set_initial_time(start_datetime)
+    # Perform harmonic analysis
     amp, pha = uptide.harmonic_analysis(tide, data["Sea Level"],
                                         (data.index - start_datetime).total_seconds())
     return amp, pha
@@ -104,13 +135,14 @@ def get_longest_contiguous_data(data):
     """"Extract Longest Continuous Segment of Sea Level Data"""
     # https://stackoverflow.com/questions/41494444/pandas-find-longest-stretch-without-nan-values
     a = data["Sea Level"].values
+    # Create a mask for NaN
     m = np.concatenate(( [True], np.isnan(a), [True] ))
     ss = np.flatnonzero(m[1:] != m[:-1]).reshape(-1,2)
     start,stop = ss[(ss[:,1] - ss[:,0]).argmax()]
     data = data.reset_index()
-    range = data.iloc[start:stop]
-    range = range.set_index('datetime')
-    return range
+    datarange = data.iloc[start:stop]
+    datarange = datarange.set_index('datetime')
+    return datarange
 
 if __name__ == '__main__':
 
